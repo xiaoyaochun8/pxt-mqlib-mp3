43d3854aa75a5cb7382587327a8f46ecade1b935
Barry.C <wjyzxhm888@163.com>
2025-11-30 18:45:28 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -30,6 +30,57 @@ namespace mqlib {
         } else if (n == 3) {
             buf[6] = 0x03
             buf[7] = 0x0f
+        } else if (n == 4) {
+            buf[6] = 0x04
+            buf[7] = 0x10
+        } else if (n == 5) {
+            buf[6] = 0x05
+            buf[7] = 0x11
+        } else if (n == 6) {
+            buf[6] = 0x06
+            buf[7] = 0x12
+        } else if (n == 7) {
+            buf[6] = 0x07
+            buf[7] = 0x13
+        } else if (n == 8) {
+            buf[6] = 0x08
+            buf[7] = 0x14
+        } else if (n == 9) {
+            buf[6] = 0x09
+            buf[7] = 0x15
+        } else if (n == 10) {
+            buf[6] = 0x0a
+            buf[7] = 0x16
+        } else if (n == 11) {
+            buf[6] = 0x0b
+            buf[7] = 0x17
+        } else if (n == 12) {
+            buf[6] = 0x0c
+            buf[7] = 0x18
+        } else if (n == 13) {
+            buf[6] = 0x0d
+            buf[7] = 0x19
+        } else if (n == 14) {
+            buf[6] = 0x0e
+            buf[7] = 0x1a
+        } else if (n == 15) {
+            buf[6] = 0x0f
+            buf[7] = 0x1b
+        } else if (n == 16) {
+            buf[6] = 0x10
+            buf[7] = 0x1c
+        } else if (n == 17) {
+            buf[6] = 0x11
+            buf[7] = 0x1d
+        } else if (n == 18) {
+            buf[6] = 0x12
+            buf[7] = 0x1e
+        } else if (n == 19) {
+            buf[6] = 0x13
+            buf[7] = 0x1f
+        } else if (n == 20) {
+            buf[6] = 0x14
+            buf[7] = 0x20
         }
         serial.writeBuffer(buf)
     }
